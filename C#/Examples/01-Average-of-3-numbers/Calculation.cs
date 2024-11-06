d71d53ab6fbc661358bba9061b5a5998a32b551a
Mohammad Ahmadzadeh <55351260+mohammadbtc100@users.noreply.github.com>
2024-11-06 19:53:18 +0330

    Session 4

---
+++ C#/Examples/01-Average-of-3-numbers/Calculation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp1
+{
+    internal class Calculation
+    {
+        public static int read()
+        {
+            string value;
+            int x;
+            Console.Write("Enter a num : ");
+            value = Console.ReadLine();
+            //string to int
+            x = int.Parse(value);
+            return x;
+        }
+        public static float average(int a, int b, int c)
+        {
+            float avg;
+            //type casting
+            avg = (float)(a + b + c) / 3;
+            return avg;
+        }
+        public static void printAverage(int a, int b, int c, float avg)
+        {
+            //string formating
+            string s = string.Format("Average of {0},{1} and {2} equal by {3}"
+                , a, b, c, avg.ToString("F2"));
+            Console.WriteLine(s);
+        }
+        public void aboutUs() {
+            Console.WriteLine("Computer Engineering Students of Skill National University\r\n...");
+        }
+    }
+}
---
+++ C#/Examples/01-Average-of-3-numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialization
+            int a = Calculation.read();
+            int b = Calculation.read();
+            int c = Calculation.read();
+            //Calculation
+            float avg = Calculation.average(a,b,c);
+            //Output
+            Calculation.printAverage(a,b,c,avg);
+
+            Calculation obj=new Calculation();
+            obj.aboutUs();
+
+            Console.ReadKey();
+        }
+    }
+}
