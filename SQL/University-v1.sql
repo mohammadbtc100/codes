2ef4cbfa6fa46795ac5bba087e5da5b042d5ad30
Mohammad Ahmadzadeh <55351260+mohammadbtc100@users.noreply.github.com>
2024-11-27 13:34:11 +0330

    s1

---
+++ SQL/University-v1.sql
@@ -0,0 +1,52 @@
+/*new query*/
+create database uniDb;
+use uniDb;
+create table students(
+	sid bigint identity(1,1) primary key,
+	name nvarchar(50) not null,
+	field nvarchar(50)
+);
+/*Add*/
+insert into students(name,field) Values('Ali','Com');
+insert into students(name,field) Values('Reza','Com');
+/*Show*/
+select * from students;
+
+/*Courses*/
+create table courses(
+	cid int identity(1,1) primary key,
+	name nvarchar(50) not null,
+	field nvarchar(50)
+);
+insert into courses(name,field) values('Db','com');
+insert into courses(name,field) values('Ds','com');
+insert into courses(name,field) values('Ad','com');
+select * from courses;
+
+/*Teacher*/
+create table teachers(
+	tid int identity(100,10) primary key,
+	name nvarchar(50),
+	field nvarchar(50)
+);
+insert into teachers(name,field) values('Ab','com');
+insert into teachers(name,field) values('Pa','com');
+select * from teachers;
+
+/*Course Presentation*/
+create table present(
+	cid int references courses(cid),
+	tid int references teachers(tid),
+	term int,
+	[day] int,
+	[time] char(5),
+);
+select * from present;
+
+/*Unit Selection*/
+create table selection(
+	[sid] bigint references students([sid]),
+	cid int references courses(cid),
+	term int
+);
+select * from selection;
