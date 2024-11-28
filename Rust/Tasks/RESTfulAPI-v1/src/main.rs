ba33f6c7b0db432fcbeac559b406ac22b6954c98
Mohammad Ahmadzadeh <55351260+mohammadbtc100@users.noreply.github.com>
2024-11-28 20:25:48 +0330

    rust of a love

---
+++ Rust/Tasks/RESTfulAPI-v1/src/main.rs
@@ -0,0 +1,29 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Skill National University(SNU), Minab Branch")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey guys!")
+}
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Actix-web RESTful API(HTTP Server)");
+    eprint!("Server running on 127.0.0.1:8080");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
