d0a612bda238f33018ee099bdfd8b7ba66876159
Mohammad Ahmadzadeh <55351260+mohammadbtc100@users.noreply.github.com>
2024-11-29 23:04:48 +0330

    Update main.rs

--- Rust/Algorithms/src/main.rs
+++ Rust/Algorithms/src/main.rs
@@ -1,12 +1,14 @@
-//Mohammad Ahmadzadeh
-
+mod fibo;
+use fibo;
 
 fn main() {
+    fibo::fibonacci(5);
     let n=8;
+    let value:u32;
     //Series printing
-    let mut value = fibo(n, true);
-    println!("\nFibo({0}) last Term is {1}",n,value);
+    //let mut value = fibo(n, true);
+    //println!("\nFibo({0}) last Term is {1}",n,value);
 
-    value = fibonacci(n);
+    value = fibo::fibonacci(n);
     println!("Fibonacci({0}): {1}", n,value);
 }
