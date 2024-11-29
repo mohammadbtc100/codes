a2a59cdda716fde1258f7b607c5aaba2530a0095
Mohammad Ahmadzadeh <55351260+mohammadbtc100@users.noreply.github.com>
2024-11-29 17:44:57 +0330

    ok

---
+++ Rust/Algorithms/src/main.rs
@@ -0,0 +1,37 @@
+//N0n Recursive
+fn fibo(n:u32,print:bool) -> u32{
+    let mut i=2;
+    let mut a:u32=0;
+    let mut b:u32=1;
+    let mut c:u32 = 1;
+    if print== true {
+        print!("Fibo Series : {0} {1} ",a,b);
+    }
+    while i<=n {
+        c=a+b;
+        if print== true{
+            print!("{} ",c);
+        }
+        a=b;
+        b=c;
+        i=i+1;
+    }
+    c
+}
+//Recursive
+fn fibonacci(mut n: u32) -> u32 {
+    match n {
+        0 => panic!("Zero is not a Right argument"),
+        1 | 2=> 1,
+        _ => fibonacci(n-1)+fibonacci(n-2)
+    }
+}
+fn main() {
+    let n=8;
+    //Print Series
+    let mut value = fibo(n, true);
+    println!("\nFibo({0}) last Term is {1}",n,value);
+
+    value = fibonacci(n);
+    println!("Fibonacci({0}): {1}", n,value);
+}
