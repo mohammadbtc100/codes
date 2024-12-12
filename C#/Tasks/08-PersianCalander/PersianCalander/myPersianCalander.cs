564f3dab29f6979eabbaed244a67e8c8beb4c14c
Mohammad Ahmadzadeh <55351260+mohammadbtc100@users.noreply.github.com>
2024-12-12 14:36:20 +0330

    Persian Calendar

---
+++ C#/Tasks/08-PersianCalander/PersianCalander/myPersianCalander.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+namespace mypersianCalander
+{
+    public class myPersianCalander
+    {
+        PersianCalendar pc;
+        public myPersianCalander()
+        {
+            pc = new PersianCalendar();
+        }
+        public int day() { return pc.GetDayOfMonth(DateTime.Now); }
+        public int month() { return pc.GetMonth(DateTime.Now); }
+        public int year() { return pc.GetYear(DateTime.Now); }
+        public string date() { return year().ToString() + "/" + month().ToString() + "/" + day().ToString(); }
+        public int getYear(string strDate) { return int.Parse(strDate.Substring(0, 4)); }
+        public int getMonth(string strDate) { return int.Parse(strDate.Substring(5, 2)); }
+        public int getDay(string strDate) { return int.Parse(strDate.Substring(8, 2)); }
+    }
+}
